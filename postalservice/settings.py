4f40f1bc60c72db18abad192bbd32acc2142dc34
Q358 <mwquinn38@gmail.com>
2022-03-23 03:59:42 -0400

    Updated Settings / Environment Vars

--- postalservice/settings.py
+++ postalservice/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pji(tp&t40s=m9^1)_399zouu&wgw628qsdl$r&hmola0t$ka_'
+# SECRET_KEY = 'django-insecure-pji(tp&t40s=m9^1)_399zouu&wgw628qsdl$r&hmola0t$ka_'
+SECRET_KEY = os.environ.get('CATHOLICQ_PRODUCTION_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['catholicqueue.herokuapp.com']
 
 
 # Application definition
--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# from django.utils import timezone
+# date = models.DateTimeField(default=timezone.now)
+
+class Hour(models.Model):
+    lineLength = models.IntegerField()
+    busynessLevel = models.CharField()
+    isCurrent = models.BooleanField()
+
+class Day(models.Model):
+    month = models.IntegerField()
+    day = models.IntegerField()
+    dayOfWeek = models.IntegerField()
+
+class Week(models.Model):
+    weekStartDay = models.ForeignKey(Day)

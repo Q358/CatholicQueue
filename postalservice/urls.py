4dcab0ec7c3910e1def0c39e5d86abd809e317c5
Q358 <mwquinn38@gmail.com>
2022-03-26 23:59:11 -0400

    no change

--- postalservice/urls.py
+++ postalservice/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('', include('test_app.urls')),
     path('__debug__/', include('debug_toolbar.urls')),

222a1464de2d9c26cf03122cba436dbd7d1b85de
Attorri <attorri@cua.edu>
2022-03-28 16:24:21 -0400

    img

--- postalservice/settings.py
+++ postalservice/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000','catholicqueue.herokuapp.com']
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Application definition
 
 INSTALLED_APPS = [
--- test_app/models.py
+++ test_app/models.py
@@ -7,6 +7,7 @@ class Hour(models.Model):
     lineLength = models.IntegerField()
     busynessLevel = models.CharField(max_length=100)
     isCurrent = models.BooleanField()
+    hourofDay = models.IntegerField()
 
     # Shows what will show up when queried
     def __str__(self):
@@ -19,3 +20,10 @@ class Day(models.Model):
 
 class Week(models.Model):
     weekStartDay = models.ForeignKey(Day, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to = 'images/')
+
+def __str__(self):
+    return self.title

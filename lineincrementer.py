a3890ded23f3babd0c98cf168e6626d5faec651b
Q358 <mwquinn38@gmail.com>
2022-03-31 00:50:39 -0400

    Made model changes, changed incrementer, added isOpen, day and week arrays

--- lineincrementer.py
+++ lineincrementer.py
@@ -1,26 +1,40 @@
-from test_app.models import Hour
+from time import perf_counter
+from tracemalloc import start
+from test_app.models import Queue, Hour, Day, Week, Month
 from datetime import *
+
 clock = time.CLOCK_REALTIME
+startTime = None
+line = Queue.objects.first()
 currentHour = Hour.objects.filter(isCurrent=True).first()
 line = currentHour.lineLength
+openTime = 10
+closeTime = 17
 
 while(1==1):
-	if(clock.hour != currentHour.hourOfDay):
-		# Makes a new hour object
-		currentHour.isCurrent = False
-		currentHour.save()
-		currentHour = Hour(lineLength = line, busynessLevel = "Is Basically Empty", isCurrent = True, hourOfDay = clock.hour)
-		currentHour.save()
-	line = currentHour.lineLength
-	if(line > 0):
-		# Starts decrementing lineLength every 5 mins
-		timer
-		# Sets busynessLevel - might be better to do this in the html
-		#setBusyness(currentHour, line)
-	if(buttonPushed):
-		currentHour = Hour.objects.filter(isCurrent=True).first()
-		currentHour.lineLength += 1
-		currentHour.save()
+	if(date.weekday >= 5 or clock.time < openTime or clock.time > closeTime):
+		line = 0
+		currentHour.isOpen = False
+	else:
+		if(clock.hour != currentHour.hourOfDay):
+			# Makes a new hour object
+			currentHour.isCurrent = False
+			currentHour.save()
+			currentHour = Hour(lineLength = line, busynessLevel = "Is Basically Empty", isCurrent = True, hourOfDay = clock.hour)
+			currentHour.save()
+		line = currentHour.lineLength
+		if(line > 0):
+			# Starts decrementing lineLength every 5 mins
+			if(startTime == None):
+				startTime = clock.minute
+			else if(time.perf_counter() - time.):
+				
+			# Sets busynessLevel - might be better to do this in the html
+			#setBusyness(currentHour, line)
+		if(buttonPushed):
+			currentHour = Hour.objects.filter(isCurrent=True).first()
+			currentHour.lineLength += 1
+			currentHour.save()
 
 
 def setBusyness(hour, line):
--- test_app/models.py
+++ test_app/models.py
@@ -3,15 +3,19 @@ from django.db import models
 # from django.utils import timezone
 # date = models.DateTimeField(default=timezone.now)
 
+class Queue(models.Model):
+    length = models.IntegerField()
+
 class Hour(models.Model):
     lineLength = models.IntegerField()
     busynessLevel = models.CharField(max_length=100)
     isCurrent = models.BooleanField()
     hourOfDay = models.IntegerField()
+    isOpen = models.BooleanField()
 
     # Shows what will show up when queried
     def __str__(self):
-        return self.busynessLevel
+        return self.hourOfDay
 
 class Day(models.Model):
     month = models.IntegerField()
@@ -19,7 +23,10 @@ class Day(models.Model):
     dayOfWeek = models.IntegerField()
 
 class Week(models.Model):
-    weekStartDay = models.ForeignKey(Day, on_delete=models.CASCADE)
+    days = [models.ForeignKey(Day, on_delete=models.CASCADE)]
+
+class Month(models.Model):
+    weeks = [models.ForeignKey(Week, on_delete=models.CASCADE)]
 
 class Post(models.Model):
     title = models.TextField()
